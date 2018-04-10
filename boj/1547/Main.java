fa1c39d41fbbe0f76feb9e33c9f8d00bb6a54473
nhouyng <dlwhdgns04@gmail.com>
2018-04-11 00:31:58 +0900

    ADD boj/1547/Main.java

---
+++ boj/1547/Main.java
@@ -0,0 +1,28 @@
+package simul.boj1547;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int now = 1;
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == now) now = b;
+			else if(b == now) now = a;
+		}
+		
+		System.out.println(now);
+	}
+}
